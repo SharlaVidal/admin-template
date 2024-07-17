4577dfe0757a41f2dfef3ab1c2381f5e545f7d75
SharlaVidal <144283288+SharlaVidal@users.noreply.github.com>
2024-07-17 10:01:28 -0300

    projeto painel administrativo

---
+++ src/components/auth/ForcarAutenticacao.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Head from 'next/head'
+import Image from 'next/image'
+import router from 'next/router'
+import loading from '../../../public/images/loading.gif'
+import useAuth from '../../data/hook/useAuth'
+
+export default function ForcarAutenticacao(props) {
+
+    const { usuario, carregando } = useAuth()
+
+    function renderizarConteudo() {
+        return (
+            <>
+                <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                if(!document.cookie?.includes("admin-template-cod3er-auth")) {
+                                    window.location.href = "/autenticacao"
+                                }
+                            `
+                        }}
+                    />
+                </Head>
+                {props.children}
+            </>
+        )
+    }
+
+    function renderizarCarregando() {
+        return (
+            <div className={`
+                flex justify-center items-center h-screen
+            `}>
+                <Image src={loading} />
+            </div>
+        )
+    }
+
+    if(!carregando && usuario?.email) {
+        return renderizarConteudo()
+    } else if(carregando) {
+        return renderizarCarregando()
+    } else {
+        router.push('/autenticacao')
+        return null
+    }
+}
